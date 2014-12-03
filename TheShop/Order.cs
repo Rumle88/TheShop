c4f83f7548a284e10495b0a9d9146dfc63e97063
Rumle88 <grisriu@hotmail.com>
2014-12-03 12:53:58 +0100

    werw

---
+++ TheShop/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheShop
+{
+    public class Order
+    {
+        public int OrderId {get;set;}
+        public string OrderName { get; set;}
+        public string OrderInfo { get; set; }
+        public string OrderType { get; set; }
+        public int OrderFlag { get; set; }
+    }
+}
---
+++ TheShop/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheShop
+{
+    public class Product
+    {
+       private int proid = 0;
+       public void setOrderid(int id)
+       {
+           if (id > 0)
+           {
+               proid = id;
+           }
+       }
+       public int ProId { get; set;}
+	   public string ProName {get; set;}
+	   public string Info {get; set;}
+       public string Proinfo {get; set;}
+	   public int ProPrice {get; set;}
+       public string ProType {get; set;}
+    }
+}
